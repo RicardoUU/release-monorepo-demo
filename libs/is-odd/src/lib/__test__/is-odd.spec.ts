1a4f97b09577ac019cc961fa96ca3ffb2deb2cfb
Ricardo <RicardoCzj@foxmail.com>
2024-01-18 12:21:48 +0800

    feat: test

--- libs/is-odd/src/lib/__test__/is-odd.spec.ts
+++ libs/is-odd/src/lib/__test__/is-odd.spec.ts
@@ -2,6 +2,6 @@ import { isOdd } from '../is-odd';
 
 describe('eslintCastlery', () => {
   it('should work', () => {
-    expect(isOdd(1)).toEqual(true);
+    expect(isOdd(1)).toEqual(false);
   });
 });
